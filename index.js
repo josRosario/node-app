b6cda807b7d37e5311d38436e0b94841ffa3fb09
josRosario <jrosario@intellisysdcorp.com>
2025-01-24 17:12:50 -0400

    description

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Node Server is running. hello hello Yay!! 🌟');
+    res.send('Node Server is running. Nodejs is the best of best of all the time Yay!! 🌟');
 });
 
 app.listen(3000, () => {
